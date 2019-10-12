94001fb25c435460d63928df0787aa313c02839d
Puneet <puneettiwari61@gmail.com>
2019-10-12 14:17:44 +0530

    first

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+  $('button').click(function(){
+   const name = $('#nameid').val();
+   
+   $('.imgchange').attr('src',`https://joeschmoe.io/api/v1/${name}`);
+   const b = $('.imgchange');
+   console.log(b);
+
+  });
+ });
